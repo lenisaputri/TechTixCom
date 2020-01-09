4f5dbddbcb3ef4a5cc1da48f6da1b939e76ac155
Shella <dimasshella04@gmail.com>
2020-01-09 10:02:06 +0700

    tambah icon level

--- module/index.php
+++ module/index.php
@@ -192,8 +192,8 @@ switch ($level) {
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">LEVEL</span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+              <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo ($namaUser); ?></span>
               </a>
               
               <!-- Dropdown - User Information -->

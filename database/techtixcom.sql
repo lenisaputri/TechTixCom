e7da3e8e5a063fca7729693abe09f812ae198610
lenisaputri <lenisaputri98@gmail.com>
2020-01-09 09:06:32 +0700

    database

--- database/techtixcom.sql
+++ database/techtixcom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 08, 2020 at 04:29 AM
+-- Generation Time: Jan 09, 2020 at 03:03 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -30,14 +30,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tabel_admin` (
   `id_admin` int(30) NOT NULL,
-  `nama_admin` varchar(50) NOT NULL,
-  `nik` int(20) NOT NULL,
-  `alamat` text NOT NULL,
-  `no_telp` int(12) NOT NULL,
   `id_user` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nik` int(20) NOT NULL,
+  `jabatan` varchar(60) NOT NULL,
   `status_aktif` enum('ya','tidak') NOT NULL,
   `waktu_edit` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tabel_admin`
+--
+
+INSERT INTO `tabel_admin` (`id_admin`, `id_user`, `nama`, `nik`, `jabatan`, `status_aktif`, `waktu_edit`) VALUES
+(1, 3, 'admin1', 1234567, 'admin', 'ya', '2020-01-09 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -47,14 +53,20 @@ CREATE TABLE `tabel_admin` (
 
 CREATE TABLE `tabel_operator` (
   `id_operator` int(30) NOT NULL,
-  `nama_operator` varchar(50) NOT NULL,
-  `nik` int(20) NOT NULL,
-  `alamat` text NOT NULL,
-  `no_telp` int(12) NOT NULL,
   `id_user` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nik` int(20) NOT NULL,
+  `jabatan` varchar(60) NOT NULL,
   `status_aktif` enum('ya','tidak') NOT NULL,
   `waktu_edit` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tabel_operator`
+--
+
+INSERT INTO `tabel_operator` (`id_operator`, `id_user`, `nama`, `nik`, `jabatan`, `status_aktif`, `waktu_edit`) VALUES
+(1, 1, 'operator1', 1234567, 'operator1', 'ya', '2020-01-09 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -64,14 +76,20 @@ CREATE TABLE `tabel_operator` (
 
 CREATE TABLE `tabel_supervisor` (
   `id_supervisor` int(30) NOT NULL,
-  `nama_supervisor` varchar(50) NOT NULL,
-  `nik` int(20) NOT NULL,
-  `alamat` text NOT NULL,
-  `no_telp` int(12) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `waktu_edit` datetime NOT NULL,
-  `status_aktif` enum('ya','tidak') NOT NULL
+  `nama` varchar(50) NOT NULL,
+  `nik` int(20) NOT NULL,
+  `jabatan` varchar(60) NOT NULL,
+  `status_aktif` enum('ya','tidak') NOT NULL,
+  `waktu_edit` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tabel_supervisor`
+--
+
+INSERT INTO `tabel_supervisor` (`id_supervisor`, `id_user`, `nama`, `nik`, `jabatan`, `status_aktif`, `waktu_edit`) VALUES
+(1, 2, 'supervisor1', 1234567, 'supervisor', 'ya', '2020-01-09 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -87,6 +105,15 @@ CREATE TABLE `tabel_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `tabel_user`
+--
+
+INSERT INTO `tabel_user` (`id_user`, `username`, `password`, `level`) VALUES
+(1, 'operator', 'operator', 'operator'),
+(2, 'supervisor', 'supervisor', 'supervisor'),
+(3, 'admin', 'admin', 'admin');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -94,19 +121,50 @@ CREATE TABLE `tabel_user` (
 -- Indexes for table `tabel_admin`
 --
 ALTER TABLE `tabel_admin`
-  ADD PRIMARY KEY (`id_admin`);
+  ADD PRIMARY KEY (`id_admin`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `tabel_operator`
 --
 ALTER TABLE `tabel_operator`
-  ADD PRIMARY KEY (`id_operator`);
+  ADD PRIMARY KEY (`id_operator`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `tabel_supervisor`
 --
 ALTER TABLE `tabel_supervisor`
-  ADD PRIMARY KEY (`id_supervisor`);
+  ADD PRIMARY KEY (`id_supervisor`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `tabel_user`
+--
+ALTER TABLE `tabel_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tabel_admin`
+--
+ALTER TABLE `tabel_admin`
+  ADD CONSTRAINT `tabel_admin_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tabel_user` (`id_user`);
+
+--
+-- Constraints for table `tabel_operator`
+--
+ALTER TABLE `tabel_operator`
+  ADD CONSTRAINT `tabel_operator_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tabel_user` (`id_user`);
+
+--
+-- Constraints for table `tabel_supervisor`
+--
+ALTER TABLE `tabel_supervisor`
+  ADD CONSTRAINT `tabel_supervisor_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `tabel_user` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

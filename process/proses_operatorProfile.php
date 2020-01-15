46d72ac03427a32d9bfcc4fbe061aa808bc6047f
lenisaputri <lenisaputri98@gmail.com>
2020-01-15 11:11:34 +0700

    proses operator tampil

--- process/proses_operatorProfile.php
+++ process/proses_operatorProfile.php
@@ -2,14 +2,15 @@
 include "../config/connection.php";
 
 function tampilProfilOperator($con, $idUser){
-    $tampilProfilOperator = "SELECT * FROM tabel_operator td INNER JOIN tabel_user tu ON tu.username = td.nik where tu.id_user = '$idUser'";
+    
+    $tampilProfilOperator = "SELECT a.*, b.nama AS nama_jabatan, c.* FROM tabel_operator a, tabel_jabatan b , tabel_user c WHERE a.id_jabatan = b.id_jabatan AND c.username = a.nik AND c.id_user = '$idUser'";
     $resultTampilProfilOperator = mysqli_query($con, $tampilProfilOperator);
     return $resultTampilProfilOperator;
    }
 
-function tampilJabatan($con){
-    $tampilJabatan="select * from tabel_jabatan";
-    $resultJabatan = mysqli_query($con, $tampilJabatan);
-    return $resultJabatan;
-}
+// function tampilJabatan($con){
+//     $tampilJabatan="SELECT a.*, b.nama AS nama_jabatan, c.* FROM tabel_operator a, tabel_jabatan b , tabel_user c WHERE a.id_jabatan = b.id_jabatan AND c.username = a.nik AND c.id_user = '$idUser'";
+//     $resultJabatan = mysqli_query($con, $tampilJabatan);
+//     return $resultJabatan;
+// }
 ?>

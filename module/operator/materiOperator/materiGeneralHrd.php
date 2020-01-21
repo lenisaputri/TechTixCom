d9b1320ce9c4b48a88ee1b40fa3e4453f01a7dd9
Shella <dimasshella04@gmail.com>
2020-01-21 11:11:29 +0700

    materi sudah tinggal upload

--- module/operator/materiOperator/materiGeneralHrd.php
+++ module/operator/materiOperator/materiGeneralHrd.php
@@ -19,53 +19,74 @@
                     <div class="panel-heading" role="tab" id="headingMateriOne">
                         <h4 class="panel-title">
                             <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseMateriOne" aria-expanded="true" aria-controls="collapseMateriOne">
-                                <i class="fas fa-fw fa-plus"></i>
                                 Materi General HRD 1
+                                <i class="more-less glyphicon fas fa-fw fa-plus"></i>
                             </a>
                         </h4>
                     </div>
                     <div id="collapseMateriOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingMateriOne">
                         <div class="panel-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.</p>
+                            <div>
+                            <img src="../img/pdf.jpg" height="150px" width="150px;">
+                            <p>Download dengan klik button download</p>
+                            </div>
+                            <div>
+                                <i class="fas fa-fw fa-download"></i>
+                                <button type="button" class="btn btn-primary">Download</button>
+                            </div>
+                            <br>
                         </div>
                     </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="headingMateriTwo">
+                        <h4 class="panel-title">
+                            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseMateriTwo" aria-expanded="false" aria-controls="collapseMateriTwo">
+                                <i class="more-less glyphicon fas fa-fw fa-plus"></i>
+                                Materi General HRD 2
+                            </a>
+                        </h4>
                     </div>
-                    
-                    <div class="panel panel-default">
-                        <div class="panel-heading" role="tab" id="headingMateriTwo">
-                            <h4 class="panel-title">
-                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseMateriTwo" aria-expanded="false" aria-controls="collapseMateriTwo">
-                                    <i class="fas fa-fw fa-plus"></i>
-                                    Materi General HRD 2
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapseMateriTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingMateriTwo">
-                            <div class="panel-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <div id="collapseMateriTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingMateriTwo">
+                        <div class="panel-body">
+                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
+                            <div>
+                            <img src="../img/pdf.jpg" height="150px" width="150px;">
+                            <p>Download dengan klik button download</p>
                             </div>
-                        </div>
-                        </div>
-                        
-                        <div class="panel panel-default">
-                            <div class="panel-heading" role="tab" id="headingMateriThree">
-                                <h4 class="panel-title">
-                                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseMateriThree" aria-expanded="false" aria-controls="collapseMateriThree">
-                                        <i class="fas fa-fw fa-plus"></i>
-                                        Materi General HRD 3
-                                    </a>
-                                </h4>
+                            <div>
+                            <i class="fas fa-fw fa-download"></i>
+                            <button type="button" class="btn btn-primary">Download</button>
                             </div>
-                            <div id="collapseMateriThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingMateriThree">
-                                <div class="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                </div>
-                            </div>
+                            <br>
                         </div>
-                        <!-- panel-group -->
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="headingMateriThree">
+                        <h4 class="panel-title">
+                            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseMateriThree" aria-expanded="false" aria-controls="collapseMateriThree">
+                                <i class="more-less glyphicon fas fa-fw fa-plus"></i>
+                                Materi General HRD 3
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseMateriThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingMateriThree"><div class="panel-body">
+                        <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.</p>
+                        <div>
+                        <img src="../img/pdf.jpg" height="150px" width="150px;">
+                        <p>Download dengan klik button download</p>
+                        </div>
+                        <div>
+                        <i class="fas fa-fw fa-download"></i>
+                        <button type="button" class="btn btn-primary">Download</button>
+                        </div>
+                        <br>
                     </div>
                 </div>
             </div>
+            <!-- panel-group -->
         </div>
     </div>
 </div>

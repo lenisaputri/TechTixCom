9e0c810419ff44ab232734aa02302173423ff60b
Shella <dimasshella04@gmail.com>
2020-01-21 15:00:55 +0700

    hapus profile

--- module/index.php
+++ module/index.php
@@ -277,9 +277,6 @@ switch ($level) {
               
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="index.php?module=profile">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>Profile
-                </a>
                 <a class="dropdown-item" href="index.php?module=setting">
                   <i class="fas fa-cog fa-sm fa-fw mr-2 text-gray-400"></i>Settings
                 </a>

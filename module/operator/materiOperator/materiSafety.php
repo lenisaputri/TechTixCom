4b4051a9e1d8bf9b79b0de59f6ebd070b732649d
lenisaputri <lenisaputri98@gmail.com>
2020-01-24 09:48:02 +0700

    yt

--- module/operator/materiOperator/materiSafety.php
+++ module/operator/materiOperator/materiSafety.php
@@ -49,6 +49,7 @@
                                 <span class="text">Download</span>
                             </button>
                             </div>
+                            <iframe width="560" height="315" src="<?= $row["file_materi"]?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             <br>
                         </div>
                     </div>

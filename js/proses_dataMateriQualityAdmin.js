dea649fad9a94a37987987e55d9550d4e3277b5d
Shella <dimasshella04@gmail.com>
2020-01-24 08:44:13 +0700

    memperbaiki

--- js/proses_dataMateriQualityAdmin.js
+++ js/proses_dataMateriQualityAdmin.js
@@ -1,29 +1,29 @@
 // DATA MATERI Quality EDIT  
 $(".edit-dataMateriQuality-admin").click(function () {
-    var id_materiQualityEdit = $(this).attr("id_materiQualityEdit");
+  var id_materiQualityEdit = $(this).attr("id_materiQualityEdit");
 
-    $.ajax({
-      url: "../process/proses_adminDataMateriQuality.php",
-      method: "post",
-      data: {
-        editDataMateriQuality_idMateriQuality : id_materiQualityEdit,
-      },
-      success: function (data) {
-        $("#id_materiQualityUpdate").val(id_materiQualityEdit);
-        $("#edit-dataMateriQuality").html(data);
-        $("#editDataMateriQualityModal").modal("show");
-      }
-    });
+  $.ajax({
+    url: "../process/proses_adminDataMateriQuality.php",
+    method: "post",
+    data: {
+      editDataMateriQuality_idMateriQuality : id_materiQualityEdit,
+    },
+    success: function (data) {
+      $("#id_materiQualityUpdate").val(id_materiQualityEdit);
+      $("#edit-dataMateriQuality").html(data);
+      $("#editDataMateriQualityModal").modal("show");
+    }
   });
+});
 // DATA MATERI Quality EDIT END
 
 // DATA MATERI Quality HAPUS
 
 $(".hapus-dataMateriQuality-admin").click(function () {
-  var id_materi_quality = $(this).attr("id_materi_quality");
- 
-  $('#id_materiQualityHapus').val(id_materi_quality);
-  $('#hapusDataMateriQualityModal').modal("show");
+var id_materi_quality = $(this).attr("id_materi_quality");
+
+$('#id_materiQualityHapus').val(id_materi_quality);
+$('#hapusDataMateriQualityModal').modal("show");
 })
 
 // DATA MATERI Quality HAPUS END

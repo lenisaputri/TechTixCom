0f415a5215831b486f915114ce05f87bb5435230
Shella <dimasshella04@gmail.com>
2020-01-24 09:00:58 +0700

    tambah script

--- config/scripts.php
+++ config/scripts.php
@@ -33,5 +33,9 @@
 
   <script src="../js/proses_kategoriMateriAdmin.js"></script>
   <script src="../js/proses_dataMateriSafetyAdmin.js"></script>
+  <script src="../js/proses_dataMateriQuality.js"></script>
+  <script src="../js/proses_dataMateriGeneralHrd.js"></script>
+  <script src="../js/proses_dataMateriTechnical.js"></script>
+
 
   <script src="../js/materiOperator.js"></script>

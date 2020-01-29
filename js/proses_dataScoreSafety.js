e3ad9f93580606234aa7db7888c6e041697021c5
lenisaputri <lenisaputri98@gmail.com>
2020-01-29 21:09:45 +0700

    js

--- js/proses_dataScoreSafety.js
+++ js/proses_dataScoreSafety.js
@@ -1,56 +1,56 @@
-$(function() {
+$(function () {
     $('#datepickerTanggalTraining').datepicker();
-  });
+});
 
-  $(function() {
+$(function () {
     $('#datepickerTanggalTrainingEdit').datepicker();
-  });
+});
 
 // DATA SCORE SAFETY EDIT  
 $(".edit-dataScoreSafety-admin").click(function () {
-  var id_scoreSafetyEdit = $(this).attr("id_scoreSafetyEdit");
+    var id_scoreSafetyEdit = $(this).attr("id_scoreSafetyEdit");
 
-  $.ajax({
-      url: "../process/proses_adminDataScoreSafety.php",
-      method: "post",
-      data: {
-          editDataScoreSafety_idScoreSafety: id_scoreSafetyEdit
-      },
-      success: function (data) {
-          $("#id_scoreSafetyUpdate").val(id_scoreSafetyEdit);
-          $("#edit-dataScoreSafety").html(data);
-          $("#editDataScoreSafetyModal").modal("show");
-      }
-  });
+    $.ajax({
+        url: "../process/proses_adminDataScoreSafety.php",
+        method: "post",
+        data: {
+            editDataScoreSafety_idScoreSafety: id_scoreSafetyEdit
+        },
+        success: function (data) {
+            $("#id_scoreSafetyUpdate").val(id_scoreSafetyEdit);
+            $("#edit-dataScoreSafety").html(data);
+            $("#editDataScoreSafetyModal").modal("show");
+        }
+    });
 });
 // DATA SCORE SAFETY EDIT END
 
 // DATA SCORE SAFETY HAPUS
 
 $(".hapus-dataScoreSafety-admin").click(function () {
-  var id_score_safety = $(this).attr("id_score_safety");
+    var id_score_safety = $(this).attr("id_score_safety");
 
-  $('#id_scoreSafetyHapus').val(id_score_safety);
+    $('#id_scoreSafetyHapus').val(id_score_safety);
 
-  $('#hapusDataMateriSafetyModal').modal("show");
+    $('#hapusDataMateriSafetyModal').modal("show");
 })
 
 // DATA SCORE SAFETY HAPUS END
 
 // DATA SCORE SAFETY LIHAT
 $(".info-dataScoreSafety-admin").click(function () {
-  var id_scoreSafetyInfo = $(this).attr("id_scoreSafetyInfo");
+    var id_scoreSafetyInfo = $(this).attr("id_scoreSafetyInfo");
 
-  $.ajax({
-      url: "../process/proses_adminDataScoreSafety.php",
-      method: "post",
-      data: {
-          infoDataScoreSafety_idScoreSafety: id_scoreSafetyInfo
-      },
-      success: function (data) {
-          $("#info-dataScoreSafety").html(data);
-          $("#infoDataScoreSafetyModal").modal("show");
-      }
-  });
+    $.ajax({
+        url: "../process/proses_adminDataScoreSafety.php",
+        method: "post",
+        data: {
+            infoDataScoreSafety_idScoreSafety: id_scoreSafetyInfo
+        },
+        success: function (data) {
+            $("#info-dataScoreSafety").html(data);
+            $("#infoDataScoreSafetyModal").modal("show");
+        }
+    });
 });
 // DATA SCORE SAFETY LIHAT END

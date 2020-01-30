219dff066aa6dc90601e502f0e6146f86eac4bfa
ErmiP <ermi.pristiya@gmail.com>
2020-01-30 10:50:24 +0700

    DataScore

--- process/proses_adminDataScoreSafetyDetail.php
+++ process/proses_adminDataScoreSafetyDetail.php
@@ -1,177 +1,165 @@
 <?php
-include "../config/connection.php";
+    include "../config/connection.php";
 
-function tampilScoreSafety($con)
-{
-    $tampilScoreSafety="SELECT DISTINCT(tss.id_operator), tss.id_score_safety ,tss.*, tp.id_operator, tp.nik FROM tabel_score_safety tss, tabel_operator tp
-    WHERE tss.id_operator = tp.id_operator GROUP BY tss.id_operator";
-    $resultTampilScoreSafety=mysqli_query($con, $tampilScoreSafety);
-    return $resultTampilScoreSafety;
-}
-
-function tampilScoreSafetyDate($con)
-{
-    $tampilScoreSafetyDate="SELECT DISTINCT(tss.tanggal_training), tss.id_score_safety ,tss.*, tp.id_operator, tp.nik FROM tabel_score_safety tss, tabel_operator tp
-    WHERE tss.id_operator = tp.id_operator GROUP BY tss.tanggal_training";
-    $resultTampilScoreSafetyDate=mysqli_query($con, $tampilScoreSafetyDate);
-    return $resultTampilScoreSafetyDate;
-}
-
-function tampilScoreSafetyDateEdit($con)
-{
-    $tampilScoreSafetyDateEdit="SELECT DISTINCT(tss.tanggal_training), tss.id_score_safety ,tss.*, tp.id_operator, tp.nik FROM tabel_score_safety tss, tabel_operator tp
-    WHERE tss.id_operator = tp.id_operator GROUP BY tss.tanggal_training";
-    $resultTampilScoreSafetyDateEdit=mysqli_query($con, $tampilScoreSafetyDateEdit);
-
-     $output="";
-
-    if(mysqli_num_rows($resultTampilScoreSafetyDateEdit) > 0){
-        while($rowDateSafetyDetail=mysqli_fetch_assoc($resultTampilScoreSafetyDateEdit)){
-            if($rowDateSafetyDetail["tanggal_training"]==$tanggal_trainingEdit){
-                $output.="<option value='$rowDateSafetyDetail[tanggal_training]' selected>".$rowDateSafetyDetail["tanggal_training"]."</option>";
-            }
-
-            else{
-                $output.="<option value='$rowDateSafetyDetail[tanggal_training]'>$rowDateSafetyDetail[tanggal_training]</option>";
-            }
-        }
+    function tampilScoreSafety($con)
+    {
+        $tampilScoreSafety="SELECT DISTINCT(tss.id_operator), tss.id_score_safety ,tss.*, tp.id_operator, tp.nik FROM tabel_score_safety tss, tabel_operator tp
+        WHERE tss.id_operator = tp.id_operator GROUP BY tss.id_operator";
+        $resultTampilScoreSafety=mysqli_query($con, $tampilScoreSafety);
+        return $resultTampilScoreSafety;
     }
 
-    return $output;
-}
+    function tampilScoreSafetyDate($con)
+    {
+        $tampilScoreSafetyDate="SELECT DISTINCT(tss.tanggal_training), tss.id_score_safety ,tss.*, tp.id_operator, tp.nik FROM tabel_score_safety tss, tabel_operator tp
+        WHERE tss.id_operator = tp.id_operator GROUP BY tss.tanggal_training";
+        $resultTampilScoreSafetyDate=mysqli_query($con, $tampilScoreSafetyDate);
+        return $resultTampilScoreSafetyDate;
+    }
 
+    function tampilScoreSafetyDateEdit($con)
+    {
+        $tampilScoreSafetyDateEdit="SELECT DISTINCT(tss.tanggal_training), tss.id_score_safety ,tss.*, tp.id_operator, tp.nik FROM tabel_score_safety tss, tabel_operator tp
+        WHERE tss.id_operator = tp.id_operator GROUP BY tss.tanggal_training";
+        $resultTampilScoreSafetyDateEdit=mysqli_query($con, $tampilScoreSafetyDateEdit);
+        $output="";
 
-
-if (isset($_POST["tambahDataScoreSafetyDetail"]) || isset($_POST["editDataScoreSafetyDetail"]) || isset($_POST["hapusDataScoreSafetyDetail"])){
-
-    if($_GET["module"]=="dataScoreSafetyDetail" && $_GET["act"]=="tambah"){
-
-       $query2 = "INSERT INTO tabel_score_safety_detail (
-         id_score_safety,
-         smk3,
-         ea_hira,
-         movement_forklift,
-         confined_space,
-         loto,
-         apd,
-         bbs,
-         fire_fighting,
-         wah,
-         environment,
-         p3k,
-         tanggal_training
-    )
-
-     values
-    (  '$_POST[nikOperatorDetail]',
-        '$_POST[smk3Detail]',
-        '$_POST[eaHiraDetail]',
-        '$_POST[movForkliftDetail]',
-        '$_POST[conSpaceDetail]',
-        '$_POST[lotoDetail]',
-        '$_POST[apdDetail]',
-        '$_POST[bbsDetail]',
-        '$_POST[fireFightingDetail]',
-        '$_POST[wahDetail]',
-        '$_POST[environmentDetail]',
-        '$_POST[p3kDetail]',
-        '$_POST[tanggalTrainingDetail]'
-    );";
-    
-        if(mysqli_query($con, $query2)){ 
-            header('location:../module/index.php?module=' . $_GET["module"]);
+        if(mysqli_num_rows($resultTampilScoreSafetyDateEdit) > 0){
+            while($rowDateSafetyDetail=mysqli_fetch_assoc($resultTampilScoreSafetyDateEdit)){
+                if($rowDateSafetyDetail["tanggal_training"]==$tanggal_trainingEdit){
+                    $output.="<option value='$rowDateSafetyDetail[tanggal_training]' selected>".$rowDateSafetyDetail["tanggal_training"]."</option>";
+                }
+                else{
+                    $output.="<option value='$rowDateSafetyDetail[tanggal_training]'>$rowDateSafetyDetail[tanggal_training]</option>";
+                }
+            }
         }
-        else{            
-            echo("Error description: " . mysqli_error($con));
-        }
-    } else if ($_GET["module"]=="dataScoreSafetyDetail" && $_GET["act"]=="edit"){
-        $id_scoreSafetyDetailUpdate = $_POST["id_scoreSafetyDetailUpdate"];
+        return $output;
+    }
+
+    if (isset($_POST["tambahDataScoreSafetyDetail"]) || isset($_POST["editDataScoreSafetyDetail"]) || isset($_POST["hapusDataScoreSafetyDetail"])){
+        if($_GET["module"]=="dataScoreSafetyDetail" && $_GET["act"]=="tambah"){
+            $query2 = "INSERT INTO tabel_score_safety_detail (
+                id_score_safety,
+                smk3,
+                ea_hira,
+                movement_forklift,
+                confined_space,
+                loto,
+                apd,
+                bbs,
+                fire_fighting,
+                wah,
+                environment,
+                p3k,
+                tanggal_training
+            )
+
+            values(  
+                '$_POST[nikOperatorDetail]',
+                '$_POST[smk3Detail]',
+                '$_POST[eaHiraDetail]',
+                '$_POST[movForkliftDetail]',
+                '$_POST[conSpaceDetail]',
+                '$_POST[lotoDetail]',
+                '$_POST[apdDetail]',
+                '$_POST[bbsDetail]',
+                '$_POST[fireFightingDetail]',
+                '$_POST[wahDetail]',
+                '$_POST[environmentDetail]',
+                '$_POST[p3kDetail]',
+                '$_POST[tanggalTrainingDetail]'
+            );";
         
-        $query10="UPDATE tabel_score_safety_detail
-            set
-            smk3 = '$_POST[smk3SafetyEditDetail]',
-            ea_hira = '$_POST[eaHiraSafetyEditDetail]',
-            movement_forklift = '$_POST[movForkliftSafetyEditDetail]',
-            confined_space = '$_POST[conSpaceSafetyEditDetail]',
-            loto = '$_POST[lotoSafetyEditDetail]',
-            apd = '$_POST[apdSafetyEditDetail]',
-            bbs = '$_POST[bbsSafetyEditDetail]',
-            fire_fighting = '$_POST[fireFightingSafetyEditDetail]',
-            wah = '$_POST[wahSafetyEditDetail]',
-            environment = '$_POST[environmentSafetyEditDetail]',
-            p3k = '$_POST[p3kSafetyEditDetail]',
-            tanggal_training = '$_POST[tanggalTrainingSafetyEditDetail]'
-            where id_score_safety_detail='$id_scoreSafetyDetailUpdate';";
-
-            if(mysqli_query($con,$query10)){
-
+            if(mysqli_query($con, $query2)){ 
                 header('location:../module/index.php?module=' . $_GET["module"]);
             }
-
             else{            
                 echo("Error description: " . mysqli_error($con));
             }
-    }else if ($_GET["module"]=="dataScoreSafetyDetail" && $_GET["act"]=="hapus"){
-        $idnya = $_POST['id_score_safety_detail'];
+        } 
+        else if ($_GET["module"]=="dataScoreSafetyDetail" && $_GET["act"]=="edit"){
+            $id_scoreSafetyDetailUpdate = $_POST["id_scoreSafetyDetailUpdate"];            
+            $query10="UPDATE tabel_score_safety_detail
+                set
+                smk3 = '$_POST[smk3SafetyEditDetail]',
+                ea_hira = '$_POST[eaHiraSafetyEditDetail]',
+                movement_forklift = '$_POST[movForkliftSafetyEditDetail]',
+                confined_space = '$_POST[conSpaceSafetyEditDetail]',
+                loto = '$_POST[lotoSafetyEditDetail]',
+                apd = '$_POST[apdSafetyEditDetail]',
+                bbs = '$_POST[bbsSafetyEditDetail]',
+                fire_fighting = '$_POST[fireFightingSafetyEditDetail]',
+                wah = '$_POST[wahSafetyEditDetail]',
+                environment = '$_POST[environmentSafetyEditDetail]',
+                p3k = '$_POST[p3kSafetyEditDetail]',
+                tanggal_training = '$_POST[tanggalTrainingSafetyEditDetail]'
+                where id_score_safety_detail='$id_scoreSafetyDetailUpdate';";
 
-        $queryDelete = "DELETE FROM tabel_score_safety_detail WHERE id_score_safety_detail='$idnya';";
-
-        if(mysqli_query($con,$queryDelete)){
-
-            header('location:../module/index.php?module=' . $_GET["module"]);
+                if(mysqli_query($con,$query10)){
+                    header('location:../module/index.php?module=' . $_GET["module"]);
+                }
+                else{            
+                    echo("Error description: " . mysqli_error($con));
+                }
         }
+        else if ($_GET["module"]=="dataScoreSafetyDetail" && $_GET["act"]=="hapus"){
+            $idnya = $_POST['id_score_safety_detail'];
+            $queryDelete = "DELETE FROM tabel_score_safety_detail WHERE id_score_safety_detail='$idnya';";
 
-        else{            
-            echo("Error description: " . mysqli_error($con));
+            if(mysqli_query($con,$queryDelete)){
+                header('location:../module/index.php?module=' . $_GET["module"]);
+            }
+            else{            
+                echo("Error description: " . mysqli_error($con));
+            }
         }
     }
-}
 
-// MODAL EDIT OPERATOR
-if(isset($_POST["editDataScoreSafetyDetail_idScoreSafety"])){
-    $editScoreSafetyDetail = "SELECT tssd.*, tss.*, tp.id_operator, tp.nik 
-    FROM tabel_score_safety_detail tssd, tabel_score_safety tss, tabel_operator tp 
-    WHERE tss.id_operator = tp.id_operator
-    AND tssd.id_score_safety = tss.id_score_safety
-    AND tssd.id_score_safety_detail = $_POST[editDataScoreSafetyDetail_idScoreSafety]";
+    if(isset($_POST["editDataScoreSafetyDetail_idScoreSafety"])){
+        $editScoreSafetyDetail = "SELECT tssd.*, tss.*, tp.id_operator, tp.nik 
+            FROM tabel_score_safety_detail tssd, tabel_score_safety tss, tabel_operator tp 
+            WHERE tss.id_operator = tp.id_operator
+            AND tssd.id_score_safety = tss.id_score_safety
+            AND tssd.id_score_safety_detail = $_POST[editDataScoreSafetyDetail_idScoreSafety]";
 
-    $resultEditScoreSafetyDetail = mysqli_query($con, $editScoreSafetyDetail);
-  
-    if(mysqli_num_rows($resultEditScoreSafetyDetail) > 0){
-        $i = 1;
-        while($rowEditScoreSafetyDetail=mysqli_fetch_assoc($resultEditScoreSafetyDetail)){
-            ?>
-            <div class ="row">
-                    <div class="col-sm-6">
-                    <div class="form-group row">
-                    <input type="hidden" name="id_scoreSafetyDetailUpdate" value="<?=$rowEditScoreSafetyDetail["id_score_safety_detail"]?>" > 
-                            <div class="col-sm-12">
-                                <label class="col-sm-6 small d-flex flex-column justify-content-center" for="nikSafetyEditDetail" style="font-weight: bold">NIK OPERATOR</label>
-                                <input type="number" class="form-control" placeholder="NIK OPERATOR" id="nikSafetyEditDetail" name="nikSafetyEditDetail" value="<?=$rowEditScoreSafetyDetail["nik"]?>" disabled>
+        $resultEditScoreSafetyDetail = mysqli_query($con, $editScoreSafetyDetail);
+    
+        if(mysqli_num_rows($resultEditScoreSafetyDetail) > 0){
+            $i = 1;
+            while($rowEditScoreSafetyDetail=mysqli_fetch_assoc($resultEditScoreSafetyDetail)){
+                ?>
+                    <div class ="row">
+                        <div class="col-sm-6">
+                            <div class="form-group row">
+                                <input type="hidden" name="id_scoreSafetyDetailUpdate" value="<?=$rowEditScoreSafetyDetail["id_score_safety_detail"]?>" > 
+                                <div class="col-sm-12">
+                                    <label class="col-sm-6 small d-flex flex-column justify-content-center" for="nikSafetyEditDetail" style="font-weight: bold">NIK OPERATOR</label>
+                                    <input type="number" class="form-control" placeholder="NIK OPERATOR" id="nikSafetyEditDetail" name="nikSafetyEditDetail" value="<?=$rowEditScoreSafetyDetail["nik"]?>" disabled>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div id="nikSafetyEditDetailBlank" class="col-sm-12 small d-flex flex-column justify-content-center text-danger"></div>
+                                </div>
                             </div>
-                            <div class="col-sm-12">
-                                <div id="nikSafetyEditDetailBlank" class="col-sm-12 small d-flex flex-column justify-content-center text-danger"></div>
+                            <div class='form-group row'>
+                                <div class="col-sm-12 mb-3 mb-sm-0">
+                                    <label class="col-sm-6 small d-flex flex-column justify-content-center" for="tanggalTrainingSafetyEditDetail" style="font-weight: bold">TANGGAL TRAINING</label>
+                                    <select class="custom-select" name="tanggalTrainingSafetyEditDetail"><?=tampilScoreSafetyDateEdit($con,$rowEditScoreSafetyDetail['tanggal_training'])?></select>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div id="tanggalTrainingSafetyEditDetailBlank2" class="col-sm-12 small d-flex flex-column justify-content-center text-danger"></div>
+                                </div>
                             </div>
-                        </div>
-                        <div class='form-group row'>
-                    <div class="col-sm-12 mb-3 mb-sm-0">
-                        <label class="col-sm-6 small d-flex flex-column justify-content-center" for="tanggalTrainingSafetyEditDetail" style="font-weight: bold">TANGGAL TRAINING</label>
-                        <select class="custom-select" name="tanggalTrainingSafetyEditDetail"><?=tampilScoreSafetyDateEdit($con,$rowEditScoreSafetyDetail['tanggal_training'])?></select>
-                    </div>
-                    <div class="col-sm-12">
-                        <div id="tanggalTrainingSafetyEditDetailBlank2" class="col-sm-12 small d-flex flex-column justify-content-center text-danger"></div>
-                    </div>
-                </div>
-                    <div class="form-group row">
-                            <div class="col-sm-12">
-                                <label class="col-sm-6 small d-flex flex-column justify-content-center" for="smk3SafetyEditDetail" style="font-weight: bold">SMK3</label>
-                                <input type="number" class="form-control" placeholder="NILAI SMK3" id="smk3SafetyEditDetail" name="smk3SafetyEditDetail" value="<?=$rowEditScoreSafetyDetail["smk3"]?>" required>
+                            <div class="form-group row">
+                                <div class="col-sm-12">
+                                    <label class="col-sm-6 small d-flex flex-column justify-content-center" for="smk3SafetyEditDetail" style="font-weight: bold">SMK3</label>
+                                    <input type="number" class="form-control" placeholder="NILAI SMK3" id="smk3SafetyEditDetail" name="smk3SafetyEditDetail" value="<?=$rowEditScoreSafetyDetail["smk3"]?>" required>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div id="smk3SafetyEditDetailBlank" class="col-sm-12 small d-flex flex-column justify-content-center text-danger"></div>
+                                </div>
                             </div>
-                            <div class="col-sm-12">
-                                <div id="smk3SafetyEditDetailBlank" class="col-sm-12 small d-flex flex-column justify-content-center text-danger"></div>
-                            </div>
-                        </div>
-                        <div class="form-group row">
+                            <div class="form-group row">
                                 <div class="col-sm-12">
                                     <label class="col-sm-6 small d-flex flex-column justify-content-center" for="eaHiraSafetyEditDetail" style="font-weight: bold">EA-HIRA</label>
                                     <input type="number" class="form-control" placeholder="NILAI EA-HIRA" id="eaHiraSafetyEditDetail" name="eaHiraSafetyEditDetail" value="<?=$rowEditScoreSafetyDetail["ea_hira"]?>" required>
@@ -207,9 +195,9 @@ if(isset($_POST["editDataScoreSafetyDetail_idScoreSafety"])){
                                     <div id="lotoSafetyEditDetailBlank" class="col-sm-12 small d-flex flex-column justify-content-center text-danger"></div>
                                 </div>
                             </div>
-                    </div>
-                    <div class="col-sm-6">
-                    <div class="form-group row">
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group row">
                                 <div class="col-sm-12">
                                     <label class="col-sm-6 small d-flex flex-column justify-content-center" for="apdSafetyEditDetail" style="font-weight: bold">NILAI APD</label>
                                     <input type="number" class="form-control form-control-user" placeholder="NILAI APD" id="apdSafetyEditDetail" name="apdSafetyEditDetail" value="<?=$rowEditScoreSafetyDetail["apd"]?>" required>
@@ -263,21 +251,20 @@ if(isset($_POST["editDataScoreSafetyDetail_idScoreSafety"])){
                                     <div id="p3kSafetyEditDetailBlank" class="col-sm-12 small d-flex flex-column justify-content-center text-danger"></div>
                                 </div>
                             </div>
+                        </div>
                     </div>
-            </div>
-                        <hr>
-            <?php
-            $i++;
+                    <hr>
+                <?php
+                $i++;
             }
         }
-            ?>
-            
+            ?>                
                 <div class="form-group">
                     <div class="modal-footer border-0">
                         <button class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Tutup</button>
-                        <button class="btn btn-primary" name="editDataScoreSafetyDetail" type="submit"><i class="fa fa-check"></i> Simpan</button>
+                         <button class="btn btn-primary" name="editDataScoreSafetyDetail" type="submit"><i class="fa fa-check"></i> Simpan</button>
+                    </div>
                 </div>
             <?php
-  }
-    
+    }    
 ?>

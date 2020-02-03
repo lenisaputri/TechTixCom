c92a14d52ee81ddb4cfb35cd1f966a90f25bfd66
ErmiP <ermi.pristiya@gmail.com>
2020-02-03 16:23:50 +0700

    membenahi

--- module/admin/dataScore/dataScoreSafety.php
+++ module/admin/dataScore/dataScoreSafety.php
@@ -123,34 +123,35 @@
 
                             if(mysqli_num_rows($resultTampilData) > 0){
                                 while($rowTampilData = mysqli_fetch_assoc($resultTampilData)){
-                        ?>
-                            <tr class="text-center" id-score-safety="<?php echo $rowTampilData["id_score_safety"] ?>">
-                                <td ><?php echo $index; ?></td>
-                                <td class="kategoriMateri"><?php echo $rowTampilData["nik"]; ?></td>
-                                <td class="judulMateri"><?php echo $rowTampilData["poin"]; ?></td>
-                                <td class="fileMateri"><?php echo $rowTampilData["nilai"]; ?></td>
-                                <td class="fileMateri"><?php echo $rowTampilData["tanggal_training"]; ?></td>
-                                <td> 
-                                    <button type="button" class="btn btn-warning info-dataScoreSafety-admin mb-2 mb-sm-0" data-toggle="modal" data-target="#infoDataScoreSafetyModal" id_scoreSafetyInfo="<?php echo $rowTampilData["id_score_safety"];?>">
-                                        <i class="fas fa-info-circle"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-primary edit-dataScoreSafety-admin mb-2 mb-sm-0" data-toggle="modal" data-target="#editDataScoreSafetyModal" id_scoreSafetyEdit="<?php echo $rowTampilData["id_score_safety"];?>">
-                                        <i class="fas fa-edit"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-danger hapus-dataScoreSafety-admin mb-2 mb-sm-0" data-toggle="modal" data-target="#hapusDataMateriSafetyModal" id_score_safety="<?php echo $rowTampilData["id_score_safety"];?>" >
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                        <?php 
-                            $index++;
+                                    ?>
+                                        <tr class="text-center" id-score-safety="<?php echo $rowTampilData["id_score_safety"] ?>">
+                                            <td ><?php echo $index; ?></td>
+                                            <td class="kategoriMateri"><?php echo $rowTampilData["nik"]; ?></td>
+                                            <td class="judulMateri"><?php echo $rowTampilData["poin"]; ?></td>
+                                            <td class="fileMateri"><?php echo $rowTampilData["nilai"]; ?></td>
+                                            <td class="fileMateri"><?php echo $rowTampilData["tanggal_training"]; ?></td>
+                                            <td> 
+                                                <button type="button" class="btn btn-warning info-dataScoreSafety-admin mb-2 mb-sm-0" data-toggle="modal" data-target="#infoDataScoreSafetyModal" id_scoreSafetyInfo="<?php echo $rowTampilData["id_score_safety"];?>">
+                                                    <i class="fas fa-info-circle"></i>
+                                                </button>
+                                                <button type="button" class="btn btn-primary edit-dataScoreSafety-admin mb-2 mb-sm-0" data-toggle="modal" data-target="#editDataScoreSafetyModal" id_scoreSafetyEdit="<?php echo $rowTampilData["id_score_safety"];?>">
+                                                    <i class="fas fa-edit"></i>
+                                                </button>
+                                                <button type="button" class="btn btn-danger hapus-dataScoreSafety-admin mb-2 mb-sm-0" data-toggle="modal" data-target="#hapusDataMateriSafetyModal" id_score_safety="<?php echo $rowTampilData["id_score_safety"];?>" >
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    <?php 
+                                    $index++;
+                                }
+                                ?>
+                                <?php
+                            }   
+                            else{
+                                ?>
+                                <?php
                             }
-                        ?>
-                        <?php
-                        }   else{
-                        ?>
-                        <?php
-                        }
                         ?>
                         </tbody>
                     </table>
@@ -160,56 +161,51 @@
     </div>
     <div class="modal fade" id="editDataScoreSafetyModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header d-flex justify-content-center bg-scoreSafety border-0">
-            <h5 class="modal-title text-white w-100 text-center">Edit Data Score Safety</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+            <div class="modal-content">
+                <div class="modal-header d-flex justify-content-center bg-scoreSafety border-0">
+                    <h5 class="modal-title text-white w-100 text-center">Edit Data Score Safety</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="../process/proses_adminDataScoreSafety.php?module=dataScoreSafety&act=edit" method="POST">
+                        <input type="hidden" name="id_score_safety" id="id_scoreSafetyUpdate" >
+                        <div class="container-fluid" id="edit-dataScoreSafety"></div>
+                    </form>
+                </div>
             </div>
-            <div class="modal-body">
-                <form action="../process/proses_adminDataScoreSafety.php?module=dataScoreSafety&act=edit" method="POST">
-                    <input type="hidden" name="id_score_safety" id="id_scoreSafetyUpdate" >
-                    <div class="container-fluid" id="edit-dataScoreSafety">
-
+        </div>
+    </div>
+    <div class="modal fade" id="hapusDataScoreSafetyModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <form action="../process/proses_adminDataScoreSafety.php?module=dataScoreSafety&act=hapus" method="post">
+                    <div class="modal-body pt-5 text-center">
+                        <input type="hidden" name="id_score_safety" id="id_scoreSafetyHapus" >
+                        <strong>Apakah Anda yakin?</strong>
+                    </div>
+                    <div class="pb-4 pt-4 d-flex justify-content-around">
+                        <button type="button" class="btn btn-danger mr-4 btn-batal" data-dismiss="modal">Tidak</button>
+                        <button type="submit" name="hapusDataMateriSafety" class="btn btn-success btn-ok">Ya</button>
                     </div>
                 </form>
             </div>
         </div>
-        </div>
     </div>
-        <div class="modal fade" id="hapusDataScoreSafetyModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-            <form action="../process/proses_adminDataScoreSafety.php?module=dataScoreSafety&act=hapus" method="post">
-                <div class="modal-body pt-5 text-center">
-                <input type="hidden" name="id_score_safety" id="id_scoreSafetyHapus" >
-                <strong>Apakah Anda yakin?</strong>
-                </div>
-                <div class="pb-4 pt-4 d-flex justify-content-around">
-                <button type="button" class="btn btn-danger mr-4 btn-batal" data-dismiss="modal">Tidak</button>
-                <button type="submit" name="hapusDataMateriSafety" class="btn btn-success btn-ok">Ya</button>
-                </div>
-            </form>
-            </div>
-        </div>
-        </div>
-        <div class="modal fade" id="infoDataScoreSafetyModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="infoDataScoreSafetyModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl" role="document">
-        <div class="modal-content">
-            <div class="modal-header d-flex justify-content-center bg-scoreSafetyInfo border-0">
-            <h5 class="modal-title text-white w-100 text-center">Edit Data Score Safety</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+            <div class="modal-content">
+                <div class="modal-header d-flex justify-content-center bg-scoreSafetyInfo border-0">
+                <h5 class="modal-title text-white w-100 text-center">Edit Data Score Safety</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid" id="info-dataScoreSafety"></div>
+                </div>
             </div>
-            <div class="modal-body">
-                    <div class="container-fluid" id="info-dataScoreSafety">
-
-                    </div>
-            </div>
-        </div>
         </div>
     </div>
 </body>

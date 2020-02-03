b664c5dc2d78115476bad498a80e799b11a663ee
Shella <dimasshella04@gmail.com>
2020-02-03 15:14:20 +0700

    home

--- module/admin/home.php
+++ module/admin/home.php
@@ -8,28 +8,27 @@
 <div class="row">
     <!-- Earnings (Monthly) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-md font-weight-bold text-primary text-uppercase mb-1">Data Admin</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">
-                      <?php
-            $admin = jumlah($con, "tabel_admin");
-            $rowAdmin = mysqli_fetch_assoc($admin);
-            echo $rowAdmin["jumlah"];
-          ?>
-                      </div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-user fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
+      <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-md font-weight-bold text-primary text-uppercase mb-1">Data Admin</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                <?php
+                  $admin = jumlah($con, "tabel_admin");
+                  $rowAdmin = mysqli_fetch_assoc($admin);
+                  echo $rowAdmin["jumlah"];
+                  ?>
                 </div>
               </div>
+                <div class="col-auto">
+                  <i class="fas fa-user fa-2x text-gray-300"></i>
+                </div>
+              </div>
+              </div>
             </div>
-
-            <!-- Earnings (Monthly) Card Example -->
+          </div>
+          <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
@@ -51,9 +50,8 @@
                 </div>
               </div>
             </div>
-
-<!-- Earnings (Monthly) Card Example -->
-<div class="col-xl-3 col-md-6 mb-4">
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">

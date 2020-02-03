7670ed0574e9dd5cefdcd646678e12fa58f176e0
ErmiP <ermi.pristiya@gmail.com>
2020-02-03 08:46:54 +0700

    Hrd

--- js/proses_dataScoreGeneralHrd.js
+++ js/proses_dataScoreGeneralHrd.js
@@ -46,3 +46,28 @@ $(function () {
       }
     });
   });
+  
+$(".edit-dataScoreGeneralHrdDetail-admin").click(function () {
+  var id_scoreGeneralHrdDetailEdit = $(this).attr("id_scoreGeneralHrdDetailEdit");
+
+  $.ajax({
+      url: "../process/proses_adminDataScoreGeneralHrdDetail.php",
+      method: "post",
+      data: {
+          editDataScoreGeneralHrdDetail_idScoreGeneralHrd: id_scoreGeneralHrdDetailEdit
+      },
+      success: function (data) {
+          $("#id_scoreGeneralHrdDetailUpdate").val(id_scoreGeneralHrdDetailEdit);
+          $("#edit-dataScoreGeneralHrdDetail").html(data);
+          $("#editDataScoreGeneralHrdDetailModal").modal("show");
+      }
+  });
+});
+
+$(".hapus-dataScoreGeneralHrdDetail-admin").click(function () {
+  var id_score_generalhrd_detail = $(this).attr("id_score_generalhrd_detail");
+
+  $('#id_scoreGeneralHrdDetailHapus').val(id_score_generalhrd_detail);
+
+  $('#hapusDataScoreGeneralHrdDetailModal').modal("show");
+})

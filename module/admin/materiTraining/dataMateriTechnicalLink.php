7d400ad63589617212409c5ddb86110378958a96
ErmiP <ermi.pristiya@gmail.com>
2020-02-03 16:47:10 +0700

    uwuwu

--- module/admin/materiTraining/dataMateriTechnicalLink.php
+++ module/admin/materiTraining/dataMateriTechnicalLink.php
@@ -3,9 +3,8 @@
   include "../process/proses_adminDataMateriTechnicalLink.php";
 ?>
 <body>
-<!-- Begin Page Content -->
-<div class="container-fluid" id="dataMateri">
-    <nav aria-label="breadcrumb" class="shadow">
+    <div class="container-fluid" id="dataMateri">
+        <nav aria-label="breadcrumb" class="shadow">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item">
                     <a href="index.php?module=home" ><i class="fas fa-fw fa-home"></i>
@@ -28,14 +27,12 @@
                 <h6 class="m-0 font-weight-bold text-primary">Tambah Data Materi Technical Link</h6>
             </div>
             <div class="card-body">
-                <!-- FORM MENAMBAH DATA -->
                 <form class="user" action="../process/proses_adminDataMateriTechnicalLink.php?module=dataMateriTechnicalLink&act=tambah" id="formDataMateriAdmin" method="POST">
-                <div class="form-group">
-                            <input type="text" class="form-control border-0" id="linkMateriTechnicalLink" name="linkMateriTechnicalLink" placeholder="Link Materi ..." style="width=100%" required>
-                            <div class="col-sm-6">
-                                <div id="linkMateriTechnicalLinkBlank" class="small d-flex flex-column justify-content-center text-danger"></div>
-                            </div>
-                        
+                    <div class="form-group">
+                        <input type="text" class="form-control border-0" id="linkMateriTechnicalLink" name="linkMateriTechnicalLink" placeholder="Link Materi ..." style="width=100%" required>
+                        <div class="col-sm-6">
+                            <div id="linkMateriTechnicalLinkBlank" class="small d-flex flex-column justify-content-center text-danger"></div>
+                        </div>
                     </div>
                     <hr>
                     <div class="form-group">
@@ -60,122 +57,105 @@
                     </div>
                     <hr>
                     <div class="form-group">
-                                <button type="submit" class="btn btn-success btn-icon-split" name="tambahDataMateriTechnicalLink" onclick="ValidasiTambahDataMateriTechnicalLink();">
-                                <span class="icon text-white-50">
-                                    <i class="fas fa-plus"></i>
-                                </span>
-                                <span class="text">Tambah Data Link</span>
-                            </button>
+                        <button type="submit" class="btn btn-success btn-icon-split" name="tambahDataMateriTechnicalLink" onclick="ValidasiTambahDataMateriTechnicalLink();">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-plus"></i>
+                            </span>
+                            <span class="text">Tambah Data Link</span>
+                        </button>
                     </div>
-                    <!-- NANTI DIGANTI TYPE BUTTON TAMBAH BUKA A HREF -->
                 </form>
-                <!-- PROSES FORM MENAMBAH DATA SELESAI -->
             </div>
         </div>
-        <!-- MENAMPILKAN DATA -->
         <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Data Materi Technical Link</h6>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr class="text-center">
-                            <th>No</th>
-                            <th>Kategori Materi</th>
-                            <th>Judul</th>
-                            <th>File</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                        $queryTampilData = "SELECT * FROM tabel_materi_technical WHERE tipe LIKE '%link%';";
-                        
-                        $resultTampilData = mysqli_query($con, $queryTampilData);
-                        $index = 1;
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Data Materi Technical Link</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr class="text-center">
+                                <th>No</th>
+                                <th>Kategori Materi</th>
+                                <th>Judul</th>
+                                <th>File</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                            $queryTampilData = "SELECT * FROM tabel_materi_technical WHERE tipe LIKE '%link%';";
+                            
+                            $resultTampilData = mysqli_query($con, $queryTampilData);
+                            $index = 1;
 
-                        if(mysqli_num_rows($resultTampilData) > 0){
-                            while($rowTampilData = mysqli_fetch_assoc($resultTampilData)){
-                    ?>
-                        <tr class="text-center" id-materi="<?php echo $rowTampilData["id_materi_technical"] ?>">
-                            <td ><?php echo $index; ?></td>
-                            <td class="kategoriMateri"><?php echo $rowTampilData["kategori_materi"]; ?></td>
-                            <td class="judulMateri"><?php echo $rowTampilData["judul_materi"]; ?></td>
-                            <td class="fileMateri"><?php echo $rowTampilData["file_materi"]; ?></td>
-                            <td>
-                                <button type="button" class="btn btn-primary edit-dataMateriTechnicalLink-admin mb-3 mb-sm-0" data-toggle="modal" data-target="#editDataMateriTechnicalLinkModal" id_materiTechnicalLinkEdit="<?php echo $rowTampilData["id_materi_technical"];?>">
-                                    <i class="fas fa-edit"></i>
-                                </button>
-                                <button type="button" class="btn btn-danger hapus-dataMateriTechnicalLink-admin" data-toggle="modal" data-target="#hapusDataMateriTechnicalLinkModal" id_materi_technical="<?php echo $rowTampilData["id_materi_technical"];?>">
-                                    <i class="fas fa-trash"></i>
-                                </button>
-                            </td>
-                        </tr>
-                    <?php 
-                        $index++;
+                            if(mysqli_num_rows($resultTampilData) > 0){
+                                while($rowTampilData = mysqli_fetch_assoc($resultTampilData)){
+                        ?>
+                            <tr class="text-center" id-materi="<?php echo $rowTampilData["id_materi_technical"] ?>">
+                                <td ><?php echo $index; ?></td>
+                                <td class="kategoriMateri"><?php echo $rowTampilData["kategori_materi"]; ?></td>
+                                <td class="judulMateri"><?php echo $rowTampilData["judul_materi"]; ?></td>
+                                <td class="fileMateri"><?php echo $rowTampilData["file_materi"]; ?></td>
+                                <td>
+                                    <button type="button" class="btn btn-primary edit-dataMateriTechnicalLink-admin mb-3 mb-sm-0" data-toggle="modal" data-target="#editDataMateriTechnicalLinkModal" id_materiTechnicalLinkEdit="<?php echo $rowTampilData["id_materi_technical"];?>">
+                                        <i class="fas fa-edit"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-danger hapus-dataMateriTechnicalLink-admin" data-toggle="modal" data-target="#hapusDataMateriTechnicalLinkModal" id_materi_technical="<?php echo $rowTampilData["id_materi_technical"];?>">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        <?php 
+                            $index++;
+                            }
+                        ?>
+                        <?php
+                        }   else{
+                        ?>
+                        <?php
                         }
-                    ?>
-                    <?php
-                    }   else{
-                    ?>
-                        <!-- <div>
-                            <p>Data Operator tidak tersedia</p>
-                        </div> -->
-                    <?php
-                    }
-                    ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- MENAMPILKAN DATA SELESAI-->
-</div>
-<!-- /.container-fluid -->
-
-<!-- Modal Edit Jabatan-->
-<div class="modal fade" id="editDataMateriTechnicalLinkModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl" role="document">
-      <div class="modal-content">
-        <div class="modal-header d-flex justify-content-center bg-materiTechnicalLink border-0">
-          <h5 class="modal-title text-white w-100 text-center">Edit Data Materi Technical Link</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-            <form action="../process/proses_adminDataMateriTechnicalLink.php?module=dataMateriTechnicalLink&act=edit" method="POST" enctype="multipart/form-data">
-                <input type="hidden" name="id_materi_technical" id="id_materiTechnicalLinkUpdate" >
-                <div class="container-fluid" id="edit-dataMateriTechnicalLink">
-
+                        ?>
+                        </tbody>
+                    </table>
                 </div>
-            </form>
-        </div>
-      </div>
-    </div>
-  </div>
- <!-- End Modal Edit Jabatan -->
-
-    <!-- Modal Hapus Jabatan-->
-    <div class="modal fade" id="hapusDataMateriTechnicalLinkModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-      aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <form action="../process/proses_adminDataMateriTechnicalLink.php?module=dataMateriTechnicalLink&act=hapus" method="post">
-            <div class="modal-body pt-5 text-center">
-            <input type="hidden" name="id_materi_technical" id="id_materiTechnicalLinkHapus" >
-              <strong>Apakah Anda yakin?</strong>
             </div>
-            <div class="pb-4 pt-4 d-flex justify-content-around">
-              <button type="button" class="btn btn-danger mr-4 btn-batal" data-dismiss="modal">Tidak</button>
-              <button type="submit" name="hapusDataMateriTechnicalLink" class="btn btn-success btn-ok">Ya</button>
-            </div>
-          </form>
         </div>
-      </div>
     </div>
-    <!-- End Modal Hapus Jabatan -->
+    </div>
+    <div class="modal fade" id="editDataMateriTechnicalLinkModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header d-flex justify-content-center bg-materiTechnicalLink border-0">
+                    <h5 class="modal-title text-white w-100 text-center">Edit Data Materi Technical Link</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="../process/proses_adminDataMateriTechnicalLink.php?module=dataMateriTechnicalLink&act=edit" method="POST" enctype="multipart/form-data">
+                        <input type="hidden" name="id_materi_technical" id="id_materiTechnicalLinkUpdate" >
+                        <div class="container-fluid" id="edit-dataMateriTechnicalLink"></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="hapusDataMateriTechnicalLinkModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <form action="../process/proses_adminDataMateriTechnicalLink.php?module=dataMateriTechnicalLink&act=hapus" method="post">
+                    <div class="modal-body pt-5 text-center">
+                    <input type="hidden" name="id_materi_technical" id="id_materiTechnicalLinkHapus" >
+                    <strong>Apakah Anda yakin?</strong>
+                    </div>
+                    <div class="pb-4 pt-4 d-flex justify-content-around">
+                    <button type="button" class="btn btn-danger mr-4 btn-batal" data-dismiss="modal">Tidak</button>
+                    <button type="submit" name="hapusDataMateriTechnicalLink" class="btn btn-success btn-ok">Ya</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>

bed81dc78816ff5db128a68babae1d48b9b271a0
ErmiP <ermi.pristiya@gmail.com>
2020-02-26 07:29:31 +0700

    modified

--- module/operator/home.php
+++ module/operator/home.php
@@ -3,45 +3,42 @@
   include "../process/proses_operatorProfile.php";
 ?>
 
-<!-- Begin Page Content -->
 <div class="container-fluid" id="dataKaryawan">
-    <!-- DataTales Example -->
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <center><h4 class="m-0 font-weight-bold text-primary">PROFILE KARYAWAN</h4></center>   
-        </div>
-        <div class="card-body">
-        <?php 
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <center><h4 class="m-0 font-weight-bold text-primary">PROFILE KARYAWAN</h4></center>   
+    </div>
+    <div class="card-body">
+      <?php 
         $resultTampilProfilOperator = tampilProfilOperator($con, $idUser);
         if (mysqli_num_rows($resultTampilProfilOperator) > 0){
           while ($row = mysqli_fetch_assoc($resultTampilProfilOperator)) {
             ?>
-        <div class="row">
-        <div class="col-lg-4">
-        <center><img src="../attachment/img/<?php echo ($row['foto'] == null)? 'avatar.jpeg' : $row['foto'] ; ?>" height="250px" width="250px;"></center>
-        </div>
-              <div class="col-lg-5">
-                <div class="p-5">
-                <div class="text">
-                    <h6 class="text-gray-900 mb-4">ID Karyawan : <?= $row["nik"]?> </h6>
-                  </div>
-                  <div class="text">
-                    <h6 class="text-gray-900 mb-4">Nama Karyawan : <?= $row["nama"]?> </h6>
-                  </div>
-                  <div class="text">
-                    <h6 class="text-gray-900 mb-4">Jenis Kelamin : <?= $row["jenis_kelamin"];?> </h6>
-                  </div>
-                  <div class="text">
-                    <h6 class="text-gray-900 mb-4">Jabatan : <?= $row["nama_jabatan"];?> </h6>
+              <div class="row">
+                <div class="col-lg-4">
+                <center><img src="../attachment/img/<?php echo ($row['foto'] == null)? 'avatar.jpeg' : $row['foto'] ; ?>" height="250px" width="250px;"></center>
+                </div>
+                <div class="col-lg-5">
+                  <div class="p-5">
+                    <div class="text">
+                      <h6 class="text-gray-900 mb-4">ID Karyawan : <?= $row["nik"]?> </h6>
+                    </div>
+                    <div class="text">
+                      <h6 class="text-gray-900 mb-4">Nama Karyawan : <?= $row["nama"]?> </h6>
+                    </div>
+                    <div class="text">
+                      <h6 class="text-gray-900 mb-4">Jenis Kelamin : <?= $row["jenis_kelamin"];?> </h6>
+                    </div>
+                    <div class="text">
+                      <h6 class="text-gray-900 mb-4">Jabatan : <?= $row["nama_jabatan"];?> </h6>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
             <?php
           } 
         }
-        ?>
-        </div>
+      ?>
     </div>
+  </div>
 </div>
-<!-- /.container-fluid -->
--- module/operator/materiOperator/materiSafety.php
+++ module/operator/materiOperator/materiSafety.php
@@ -26,78 +26,79 @@
                 $resultTampilMateriSafety = tampilMateriSafety($con);
                 $index=1;
                 if (mysqli_num_rows($resultTampilMateriSafety) > 0){
-            ?>
-            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                <div class="panel panel-default">
-                    <?php
-                        while ($row = mysqli_fetch_assoc($resultTampilMateriSafety)) {
-                            ?>
-                                <div class="panel-heading" role="tab" id="headingMateriOne" style="word-break: break-all;">
-                                    <h4 class="panel-title">
-                                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseMateriOne<?= $index?>" aria-expanded="true" aria-controls="collapseMateriOne<?= $index?>">
-                                            <?= $row["judul_materi"]?>
-                                            <i class="more-less glyphicon fas fa-fw fa-plus"></i>
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseMateriOne<?= $index?>" class="panel-collapse collapse ml-3" role="tabpanel" aria-labelledby="headingMateriOne<?= $index?>">
-                                    <div class="panel-body">
-                                        <div class="row">
-                                            <p class="col-sm-12 d-flex flex-column justify-content-center">KATEGORI MATERI :</p>
-                                            <div class="form-group row">
-                                                <div class="col-sm-12" style="word-break: break-all;">
-                                                    <label class="col-sm-12 d-flex flex-column justify-content-center" for="nik" style="font-weight: bold"> <?= $row["kategori_materi"]?></label>
-                                                </div>
+                    ?>
+                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                            <div class="panel panel-default">
+                                <?php
+                                    while ($row = mysqli_fetch_assoc($resultTampilMateriSafety)) {
+                                        ?>
+                                            <div class="panel-heading" role="tab" id="headingMateriOne" style="word-break: break-all;">
+                                                <h4 class="panel-title">
+                                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseMateriOne<?= $index?>" aria-expanded="true" aria-controls="collapseMateriOne<?= $index?>">
+                                                        <?= $row["judul_materi"]?>
+                                                        <i class="more-less glyphicon fas fa-fw fa-plus"></i>
+                                                    </a>
+                                                </h4>
                                             </div>
-                                            <p class="col-sm-12 d-flex flex-column justify-content-center">KETERANGAN :</p>
-                                            <div class="form-group row">
-                                                <div class="col-sm-12" style="word-break: break-all;">
-                                                    <p class="col-sm-12 d-flex flex-column justify-content-center" for="nik" style="font-weight: bold;"><?= $row["keterangan_materi"]?></p>
-                                                </div>
-                                            </div>
-                                            <?php
-                                                if($row["tipe"] == "file"){
-                                                    ?>
-                                                        <img src="../img/pdf.jpg" height="150px" width="150px;" class="d-flex flex-column justify-content-center">
-                                                        <p class="col-sm-12 d-flex flex-column justify-content-center" style="word-break: break-all;"><?= $row["file_materi"]?></p>
+                                            <div id="collapseMateriOne<?= $index?>" class="panel-collapse collapse ml-3" role="tabpanel" aria-labelledby="headingMateriOne<?= $index?>">
+                                                <div class="panel-body">
+                                                    <div class="row">
+                                                        <p class="col-sm-12 d-flex flex-column justify-content-center">KATEGORI MATERI :</p>
                                                         <div class="form-group row">
-                                                            <div class="col-sm-12 d-flex flex-column justify-content-center">
-                                                                <a class="btn btn-primary btn-icon-split" href="../process/proses_download_safety.php?file=<?=$row["file_materi"]?>">
-                                                                    <span class="icon text-white-50">
-                                                                        <i class="fas fa-fw fa-download"></i>
-                                                                    </span>
-                                                                    <span class="text">Download</span>
-                                                                </a>
+                                                            <div class="col-sm-12" style="word-break: break-all;">
+                                                                <label class="col-sm-12 d-flex flex-column justify-content-center" for="nik" style="font-weight: bold"> <?= $row["kategori_materi"]?></label>
                                                             </div>
                                                         </div>
-                                                    <?php
-                                                } 
-                                                else if($row["tipe"] == "link"){
-                                                    ?>
-                                                        <div class="embed-responsive embed-responsive-16by9 mb-3">
-                                                            <iframe class="embed-responsive-item" src="<?= $row["file_materi"]?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                                        <p class="col-sm-12 d-flex flex-column justify-content-center">KETERANGAN :</p>
+                                                        <div class="form-group row">
+                                                            <div class="col-sm-12" style="word-break: break-all;">
+                                                                <p class="col-sm-12 d-flex flex-column justify-content-center" for="nik" style="font-weight: bold;"><?= $row["keterangan_materi"]?></p>
+                                                            </div>
                                                         </div>
-                                                    <?php
-                                                }
-                                            ?>
-                                        </div>
-                                    </div>  
-                                </div>
-                            <?php
-                            $index++;
-                        }
-                    ?>
-                </div>
-            </div>
-            <?php
-                }else{
+                                                        <?php
+                                                            if($row["tipe"] == "file"){
+                                                                ?>
+                                                                    <img src="../img/pdf.jpg" height="150px" width="150px;" class="d-flex flex-column justify-content-center">
+                                                                    <p class="col-sm-12 d-flex flex-column justify-content-center" style="word-break: break-all;"><?= $row["file_materi"]?></p>
+                                                                    <div class="form-group row">
+                                                                        <div class="col-sm-12 d-flex flex-column justify-content-center">
+                                                                            <a class="btn btn-primary btn-icon-split" href="../process/proses_download_safety.php?file=<?=$row["file_materi"]?>">
+                                                                                <span class="icon text-white-50">
+                                                                                    <i class="fas fa-fw fa-download"></i>
+                                                                                </span>
+                                                                                <span class="text">Download</span>
+                                                                            </a>
+                                                                        </div>
+                                                                    </div>
+                                                                <?php
+                                                            } 
+                                                            else if($row["tipe"] == "link"){
+                                                                ?>
+                                                                    <div class="embed-responsive embed-responsive-16by9 mb-3">
+                                                                        <iframe class="embed-responsive-item" src="<?= $row["file_materi"]?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                                                    </div>
+                                                                <?php
+                                                            }
+                                                        ?>
+                                                    </div>
+                                                </div>  
+                                            </div>
+                                        <?php
+                                        $index++;
+                                    }
+                                ?>
+                            </div>
+                        </div>
+                    <?php
+                }
+                else{
                     ?>
                         <div class="text-center">
                             <p class="text-muted">Data Materi Safety kosong!!</p>
                         </div>
                     <?php
                 }
-                ?>
+            ?>
         </div>
     </div>
 </div>
